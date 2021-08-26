8652ed5ce401935b533e264f5d1eec37ccdb6e87
bamfokbq <keepthefaithkb@gmail.com>
2021-08-26 23:22:43 +0000

    Added PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const withPWA = require('next-pwa');
+
 module.exports = {
   reactStrictMode: true,
   images: {
     domain: ['theysaidso.com'],
   },
 };
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+});
