1e96009b0e38ea2a237bfaeb9cae4091215f5455
bamfokbq <keepthefaithkb@gmail.com>
2021-08-26 12:14:54 +0000

    Adjusted the font sizes

--- components/QuoteContent/QuoteContent.js
+++ components/QuoteContent/QuoteContent.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './QuoteContent.module.css';
 
-const QuoteContent = ({ author, quote, date, tags, category }) => {
+const QuoteContent = ({ author, quote, date, tags }) => {
   console.log(tags);
   return (
     <section className={styles.content}>
       <p className={styles.date}>Date: {date}</p>
       <h3 className={styles.author}>Author: {author}</h3>
       <p className={styles.quote}>{quote}</p>
-      <p className={styles.category}>Category: {category}</p>
+      <div className={styles.category}>
+        <span> Tags:</span>
+        {tags.map((tag) => (
+          <p key={tag}> {tag},</p>
+        ))}
+      </div>
     </section>
   );
 };
